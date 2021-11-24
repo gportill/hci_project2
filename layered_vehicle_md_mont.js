1b4ea96149e063899c20653431d5500ff36867c7
Gwenyth Portillo Wightman <gwenythportillowightman@Gwenyths-Air.MG8702>
2021-11-24 08:39:32 -0500

    update

--- layered_vehicle_md_mont.js
+++ layered_vehicle_md_mont.js
@@ -2,7 +2,7 @@ am5.ready(function() {
 
     // Create root element
     // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-    var root = am5.Root.new("chartdiv4");
+    var root = am5.Root.new("chartdiv10");
 
 
     // Set themes
@@ -29,101 +29,15 @@ am5.ready(function() {
 
     // TODO
     var data = [{
-    "year": "Allegany",
-    "0.5": 94,
-    "1": 66
-    }, {
-    "year": "Anne Arundel",
-    "0.5": 70,
-    "1": 36
-    }, {
     "year": "Baltimore City",
     "0.5": 47,
-    "1": 5
-    }, {
-    "year": "Baltimore County",
-    "0.5": 66,
-    "1": 28
-    }, {
-    "year": "Calvert",
-    "0.5": 89,
-    "1": 77
-    }, {
-    "year": "Caroline",
-    "0.5": 92,
-    "1": 77
-    }, {
-    "year": "Carroll",
-    "0.5": 85,
-    "1": 65
-    }, {
-    "year": "Cecil",
-    "0.5": 88,
-    "1": 71
-    }, {
-    "year": "Charles",
-    "0.5": 77,
-    "1": 58
-    }, {
-    "year": "Dorchester",
-    "0.5": 90,
-    "1": 71
-    }, {
-    "year": "Frederick",
-    "0.5": 71,
-    "1": 48
-    }, {
-    "year": "Garrett",
-    "0.5": 98,
-    "1": 85
-    }, {
-    "year": "Harford",
-    "0.5": 76,
-    "1": 44
-    }, {
-    "year": "Howard",
-    "0.5": 65,
-    "1": 28
-    }, {
-    "year": "Kent",
-    "0.5": 81,
-    "1": 62
+    "1": 5, 
+    "vehicle": 70
     }, {
     "year": "Montgomery",
     "0.5": 56,
-    "1": 20
-    }, {
-    "year": "Prince George's",
-    "0.5": 60,
-    "1": 25
-    }, {
-    "year": "Queen Anne's",
-    "0.5": 90,
-    "1": 78
-    }, {
-    "year": "St. Mary's",
-    "0.5": 82,
-    "1": 70
-    }, {
-    "year": "Somerset",
-    "0.5": 91,
-    "1": 71
-    }, {
-    "year": "Talbot",
-    "0.5": 94,
-    "1": 73
-    }, {
-    "year": "Washington",
-    "0.5": 81,
-    "1": 50
-    }, {
-    "year": "Wicomico",
-    "0.5": 78,
-    "1": 54
-    }, {
-    "year": "Worcester",
-    "0.5": 83,
-    "1": 54
+    "1": 20, 
+    "vehicle": 92.5
     }]
 
     // Create axes
@@ -147,6 +61,25 @@ am5.ready(function() {
     // Add series
     // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
 
+    var series2 = chart.series.push(am5xy.ColumnSeries.new(root, {
+      name: "% Households with Vehicle",
+      xAxis: xAxis,
+      yAxis: yAxis,
+      valueYField: "vehicle",
+      categoryXField: "year",
+      clustered: false,
+      tooltip: am5.Tooltip.new(root, {
+        labelText: "% Households with Vehicle: {valueY}%"
+      })
+    }));
+
+    series2.columns.template.setAll({
+      width: am5.percent(80),
+      tooltipY: 0
+    });
+
+    series2.data.setAll(data);
+
     var series0 = chart.series.push(am5xy.ColumnSeries.new(root, {
       name: "% People Beyond 1/2 Mile From a Grocery Store",
       xAxis: xAxis,
@@ -160,7 +93,7 @@ am5.ready(function() {
     }));
 
     series0.columns.template.setAll({
-      width: am5.percent(80),
+      width: am5.percent(65),
       tooltipY: 0
     });
 
@@ -193,6 +126,9 @@ am5.ready(function() {
     // Make stuff animate on load
     // https://www.amcharts.com/docs/v5/concepts/animations/
     chart.appear(1000, 100);
+    series1.appear();
+    series0.appear();
+    series2.appear();
     series1.appear();
     series0.appear();
 
